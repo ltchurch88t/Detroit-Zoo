ce0485f26b5f48be15c40eabad391b682867a31c
Forrest Church <ltchurch88t@gmail.com>
2018-05-13 00:11:53 -0500

    major updates
    all sections updated, closer to final iteration

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ $(function () {
   });
 });
 
-
 $(function () {
-  $('[data-toggle="popover"]').popover({ html : true })
+  $('[data-toggle="popover"]').popover({ html : true });
+  $('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    });
 })
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.hideme').each( function(i){
+            var bottomObject = $(this).position().top + $(this).outerHeight() / 4;
+            var bottomWindow = $(window).scrollTop() + $(window).height();
+            if( bottomWindow > bottomObject){
+                $(this).animate({'opacity':'1'},500);   
+            }
+        }); 
+    });
+});
